62fdf3d8c2a9068d82d13c0059f4244d65225cc1
Tudor-tech <tudor.slavu@ulbsibiu.ro>
2025-10-07 17:05:02 +0300

    lab 1 proba

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello world ");
+        //System.out.printf("Hello world ");
 
         Animal animal = new Dog();
         animal.sound();
